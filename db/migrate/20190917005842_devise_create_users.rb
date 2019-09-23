d5c494afb98d70d688eda4760fbb9b68602775a5
beaulieu527 <beaulieudev@gmail.com>
2019-09-23 16:05:07 -0400

    set up models adn removed customer  portion

--- db/migrate/20190917005842_devise_create_users.rb
+++ db/migrate/20190917005842_devise_create_users.rb
@@ -7,6 +7,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
