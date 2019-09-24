e7d804e72fcfd7bcce5bc3c5314cf649453a647f
beaulieu527 <beaulieudev@gmail.com>
2019-09-23 20:04:13 -0400

    fixed errors in controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-class ProductController < ApplicationController
+class ProductsController < ApplicationController
     def show
         @product = Product.find_by(id: params[:id])
         if !@product
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_020130) do
+ActiveRecord::Schema.define(version: 2019_09_23_235649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_020130) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "price"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
