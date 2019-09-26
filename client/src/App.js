538f269a807f36d16f73b99a84a0323e7320f76d
beaulieu527 <beaulieudev@gmail.com>
2019-09-25 21:27:52 -0400

    set up react router , provider,store, and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Provider from 'react-redux';
 import M from 'materialize-css'
+import { createStore } from 'redux';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h1>Hello</h1>
     </div>
   );
