4ce6baf83bbe29045abb627a8f1cbf5184d79800
beaulieu527 <beaulieudev@gmail.com>
2019-10-05 20:51:15 -0400

    got devise jwt working

--- client/src/components/user/Signup.js
+++ client/src/components/user/Signup.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
-import {Button,TextInput } from 'react-materialize';
+import { Button, TextInput } from 'react-materialize';
+import { userPostFetch } from '../../actions/usersActions';
 
 class Signup extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             name: '',
             email: '',
             password: '',
             address: '',
             phoneNumber: ''
-         };
+        };
     }
 
     handleOnChange = event => {
         this.setState({ [event.target.id]: event.target.value })
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.userPostFetch(this.state)
+    }
+
+
     render() {
         return (
             <div class="container">
-                <h2 style={{textAlign:'center'}}>Register</h2>
+                <h2 style={{ textAlign: 'center' }}>Register</h2>
                 <ul className="collection"></ul>
-                <br/>
-                <br/>
-                <form>
-                    <TextInput id="name" label="Name" onChange={event => this.handleOnChange(event)}/>
-                    <br/>
-                    <TextInput id="email" email validate label="Email" onChange={event => this.handleOnChange(event)}/>
-                    <br/>
+                <br />
+                <br />
+                <form onSubmit={this.handleSubmit}>
+                    <TextInput id="name" label="Name" onChange={event => this.handleOnChange(event)} />
+                    <br />
+                    <TextInput id="email" email validate label="Email" onChange={event => this.handleOnChange(event)} />
+                    <br />
                     <TextInput id="password" password label="Password" onChange={event => this.handleOnChange(event)} />
-                    <br/>
-                    <TextInput id="address" label="Address" onChange={event => this.handleOnChange(event)}/>
-                    <br/>
-                    <TextInput id="phoneNumber" label="Phone Number" onChange={event => this.handleOnChange(event)}/>
-                    <Button waves="teal" style={{marginRight: '5px'}}>
+                    <br />
+                    <TextInput id="address" label="Address" onChange={event => this.handleOnChange(event)} />
+                    <br />
+                    <TextInput id="phoneNumber" label="Phone Number" onChange={event => this.handleOnChange(event)} />
+                    <Button waves="teal" style={{ marginRight: '5px' }}>
                         Register
                     </Button>
                 </form>
@@ -42,5 +49,8 @@ class Signup extends Component {
         );
     }
 }
+const mapDispatchToProps = dispatch => ({
+    userPostFetch: userInfo => dispatch(userPostFetch(userInfo))
+})
 
-export default Signup;
+export default connect(null, mapDispatchToProps)(Signup);
