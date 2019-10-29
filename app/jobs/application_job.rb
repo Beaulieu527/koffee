9643c473efd4c4f0fc39ad043e7d2efc173a5269
beaulieu527 <beaulieudev@gmail.com>
2019-10-28 22:18:25 -0400

    fixed error

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,13 +1,13 @@
 class ApplicationJob < ActiveJob::Base
-    config.middleware.insert_before 0, Rack::Cors do
-        allow do
-          origins '*'
-          resource(
-            '*',
-            headers: :any,
-            expose: ["Authorization"],
-            methods: [:get, :patch, :put, :delete, :post, :options, :show]
-          )
-        end
-     end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #     allow do
+    #       origins '*'
+    #       resource(
+    #         '*',
+    #         headers: :any,
+    #         expose: ["Authorization"],
+    #         methods: [:get, :patch, :put, :delete, :post, :options, :show]
+    #       )
+    #     end
+    #  end
 end
